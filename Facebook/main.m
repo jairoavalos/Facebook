cf89f546e0cfacbf406d41f16952a68122d158a8
Jairo Avalos <jairo@palateapp.com>
2014-06-05 00:36:07 -0700

    First commit
    Creating the project

---
+++ Facebook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Facebook
+//
+//  Created by Jairo Avalos on 6/5/14.
+//  Copyright (c) 2014 Jairo Avalos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
