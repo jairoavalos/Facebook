07d44a6636ac8777ef57c238512d3909b1e7c546
Jairo Avalos <jairo@palateapp.com>
2014-06-05 01:34:26 -0700

    Getting some visuals down in IB

--- Facebook/AppDelegate.m
+++ Facebook/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "PostViewController.h"
 
 @implementation AppDelegate
 
@@ -14,6 +15,10 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+  
+    PostViewController *pVC = [[PostViewController alloc] init];
+    self.window.rootViewController = pVC;
+  
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
